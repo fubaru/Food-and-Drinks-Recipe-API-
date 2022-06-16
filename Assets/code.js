f70487f2120b1464bd2b7037331b9f4f3230c119
fuaus <kotingfunk@gmail.com>
2022-06-15 21:19:21 -0400

    HTML: adjust margins and static texts. create IDs. JS: defining HTML static selectors

---
+++ Assets/code.js
@@ -0,0 +1,4 @@
+// define HTMl Static Selectors
+var drinksEl = document.querySelector("#drinks")
+var drinksFormEl = document.querySelector("#drinksForm")
+// Current Searched drink cards selectors
