af1c00698bad919dcae5a491b0afa6d963dcf181
fuaus <kotingfunk@gmail.com>
2022-06-16 15:35:35 -0400

    JS: create static HTML selectors for drinks cards and its ingredients and instructions

--- Assets/code.js
+++ Assets/code.js
@@ -1,4 +1,21 @@
 // define HTMl Static Selectors
-var drinksEl = document.querySelector("#drinks")
-var drinksFormEl = document.querySelector("#drinksForm")
+var drinksEl = document.querySelector("#drinks");
+var drinksFormEl = document.querySelector("#drinksForm");
 // Current Searched drink cards selectors
+var drinkCard1El = document.querySelector("#drinkCard1");
+var drinkCard2El = document.querySelector("#drinkCard2");
+var drinkCard3El = document.querySelector("#drinkCard3");
+var drinkCard4El = document.querySelector("#drinkCard4");
+var drinkCard5El = document.querySelector("#drinkCard5");
+// ingredients selectors
+var ingredients1El = document.querySelector("#ingredients1");
+var ingredients2El = document.querySelector("#ingredients2");
+var ingredients3El = document.querySelector("#ingredients3");
+var ingredients4El = document.querySelector("#ingredients4");
+var ingredients5El = document.querySelector("#ingredients5");
+// instructions selectors
+var instructions1El = document.querySelector("#instructions1");
+var instructions1El = document.querySelector("#instructions2");
+var instructions1El = document.querySelector("#instructions3");
+var instructions1El = document.querySelector("#instructions4");
+var instructions1El = document.querySelector("#instructions5");
