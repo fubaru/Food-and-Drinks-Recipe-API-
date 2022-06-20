6088b99d9f30a6d6587f10ef3ef2e8099620571a
fuaus <kotingfunk@gmail.com>
2022-06-20 18:43:28 -0400

    new changes

--- Assets/code.js
+++ Assets/code.js
@@ -7,6 +7,10 @@ var tommyMargsBtn = document.querySelector("#tommyMargs");
 var whitecapMargsBtn = document.querySelector("#whitecapMargs");
 var strawbMargsBtn = document.querySelector("#strawbMargs");
 var melonMargsBtn = document.querySelector("#melonMargs");
+var boxcarBtn = document.querySelector("#boxcar");
+var casinoBtn = document.querySelector("#casino");
+var mimosaBtn = document.querySelector("#mimosa");
+var maitaiBtn = document.querySelector("#maitai");
 var selectEl = document.querySelector("#select");
 // var drinksFormEl = document.querySelector("#drinksForm");
 // Current Searched drink cards selectors
@@ -49,10 +53,12 @@ function populateData(event) {
         .then(function(currentData) {
             console.log(currentData);
             // defined variable with ternary opeator to not display null on page
+            var ingredients3 = currentData.drinks[0].strIngredient3 == null ? " " : currentData.drinks[0].strIngredient3
             var ingredients4 = currentData.drinks[0].strIngredient4 == null ? " " : currentData.drinks[0].strIngredient4
             var ingredients5 = currentData.drinks[0].strIngredient5 == null ? " " : currentData.drinks[0].strIngredient5
             var ingredients6 = currentData.drinks[0].strIngredient6 == null ? " " : currentData.drinks[0].strIngredient6
             var ingredients7 = currentData.drinks[0].strIngredient7 == null ? " " : currentData.drinks[0].strIngredient7
+            var measure3 = currentData.drinks[0].strMeasure3 == null ? " " : " - " + currentData.drinks[0].strMeasure3
             var measure4 = currentData.drinks[0].strMeasure4 == null ? " " : " - " + currentData.drinks[0].strMeasure4
             var measure5 = currentData.drinks[0].strMeasure5 == null ? " " : " - " + currentData.drinks[0].strMeasure5
             var measure6 = currentData.drinks[0].strMeasure6 == null ? " " : " - " + currentData.drinks[0].strMeasure6
@@ -61,7 +67,7 @@ function populateData(event) {
             // for (i=0; i < currentData.drinks.length; i++) {
                 selectEl.innerHTML =`<div class="group relative">
                 <div
-                    class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-fit lg:aspect-video">
+                    class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden lg:h-fit lg:aspect-video">
                     <img src="${currentData.drinks[0].strDrinkThumb}"
                         alt="Drinks image"
                         class="w-full h-full object-center object-cover lg:w-full lg:h-full">
@@ -80,12 +86,11 @@ function populateData(event) {
                                 <span>${currentData.drinks[0].strMeasure1}</span></li>
                                 <li>${currentData.drinks[0].strIngredient2} -
                                 <span>${currentData.drinks[0].strMeasure2}</span></li>
-                                <li>${currentData.drinks[0].strIngredient3} -
-                                <span>${currentData.drinks[0].strMeasure3}</span></li>
-                                <li>${ingredients4}<span>${measure4}<span></li>
-                                <li>${ingredients5}<span>${measure5}<span></li>
-                                <li>${ingredients6}<span>${measure6}<span></li>
-                                <li>${ingredients7}<span>${measure7}<span></li>
+                                <li>${ingredients3}<span>${measure3}</span></li>
+                                <li>${ingredients4}<span>${measure4}</span></li>
+                                <li>${ingredients5}<span>${measure5}</span></li>
+                                <li>${ingredients6}<span>${measure6}</span></li>
+                                <li>${ingredients7}<span>${measure7}</span></li>
                             </ol>
                         </p>
                         <p class="mt-1 text-sm text-gray-500" id="instructions1">${currentData.drinks[0].strInstructions}
@@ -119,3 +124,7 @@ tommyMargsBtn.addEventListener("click",populateData);
 whitecapMargsBtn.addEventListener("click",populateData);
 strawbMargsBtn.addEventListener("click",populateData);
 melonMargsBtn.addEventListener("click",populateData);
+boxcarBtn.addEventListener("click",populateData);
+casinoBtn.addEventListener("click",populateData);
+mimosaBtn.addEventListener("click",populateData);
+maitaiBtn.addEventListener("click",populateData);
