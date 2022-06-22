71778a5ae27f985a3d6654467c617a144d44f9c3
Onaysa Ali <anisa.ali.5999385@gmail.com>
2022-06-21 21:09:18 -0400

    "added the last food ApI to my JS!"

--- Food page/Assets/scrpt.js
+++ Food page/Assets/scrpt.js
@@ -32,10 +32,13 @@ var ingredients3EL = document.querySelector("#ingredients3");
 var instructions1EL = document.querySelector("#instructions1");
 var instructions2EL = document.querySelector("#instructions2");
 var instructions3EL = document.querySelector("#instructions3");
-var image1El = document.querySelector("#image1")
+var number1EL = document.querySelector("#number1");
+var number2EL = document.querySelector("#number2");
+var number3EL = document.querySelector("#number3");
+var image1El = document.querySelector("#image1");
 var recipesBtn = document.querySelector("#button-recipes");
 
-function populateData(event) {
+function populateData1() {
   var recipe1 = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`;
   fetch(recipe1)
     .then(function (response) {
@@ -43,30 +46,148 @@ function populateData(event) {
     })
     .then(function (recipe1Data) {
       console.log(recipe1Data);
-      var recipe2 = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`;
+      recipecard1El.innerHTML =`<div class="group relative">
+      <div
+          class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="${recipe1Data.meals[0].strMealThumb}"
+              alt="Front of men&#039;s Basic Tee in black."
+              class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+      </div>
+      <div class="mt-4 flex justify-between">
+          <div>
+              <h3 class="text-sm text-gray-700">
+                  <span aria-hidden="true" class="absolute inset-0" id="recipeCard1">${recipe1Data.meals[0].strMeal}</span>
+              </h3>
+              <p class="mt-1 text-sm font-medium text-gray-500" id="ingredients1">
+              <h3>Ingredients</h3>
+              <ol>
+              <li>${recipe1Data.meals[0].strIngredient1}<li>
+              <li>${recipe1Data.meals[0].strIngredient2}<li>
+              <li>${recipe1Data.meals[0].strIngredient3}<li>
+              <li>${recipe1Data.meals[0].strIngredient4}<li>
+              <li>${recipe1Data.meals[0].strIngredient5}<li>
+              <li>${recipe1Data.meals[0].strIngredient6}<li>
+              <li>${recipe1Data.meals[0].strIngredient7}<li>
+              <li>${recipe1Data.meals[0].strIngredient8}<li>
+              </ol>
+              </p>
+              <p class="mt-1 text-sm text-gray-500" id="instructions1">${recipe1Data.meals[0].strInstructions}
+              </p>
+          </div>
+      </div>
+  </div>`
+      /*var recipe2 = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`;
       fetch(recipe2)
         .then(function (response) {
           return response.json();
         })
         .then(function (recipe2Data) {
           console.log(recipe2Data);
-    
+
           var recipe3 = `https://www.themealdb.com/api/json/v1/1/random.php`;
           fetch(recipe3)
             .then(function (response) {
               return response.json();
             })
             .then(function (recipe3Data) {
-                console.log(recipe1Data);
-                console.log(recipe2Data);
-                console.log(recipe3Data);     
+              console.log(recipe1Data);
+              console.log(recipe2Data);
+              console.log(recipe3Data);
             });
-        });
+        });*/
     });
+}
+function populateData2() {
+  var recipe2 = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`;
+  fetch(recipe2)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (recipe2Data) {
+      console.log(recipe2Data);
+      recipecard2EL.innerHTML =`<div class="group relative">
+      <div
+          class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="${recipe2Data.meals[0].strMealThumb}"
+              alt="Front of men&#039;s Basic Tee in black."
+              class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+      </div>
+      <div class="mt-4 flex justify-between">
+          <div>
+              <h3 class="text-sm text-gray-700">
+                  <span aria-hidden="true" class="absolute inset-0" id="recipeCard1">${recipe2Data.meals[0].strMeal}</span>
+              </h3>
+              <p class="mt-1 text-sm font-medium text-gray-500" id="ingredients1">
+              <h3>Ingredients</h3>
+              <ol>
+              <li>${recipe2Data.meals[0].strIngredient1}<li>
+              <li>${recipe2Data.meals[0].strIngredient2}<li>
+              <li>${recipe2Data.meals[0].strIngredient3}<li>
+              <li>${recipe2Data.meals[0].strIngredient4}<li>
+              <li>${recipe2Data.meals[0].strIngredient5}<li>
+              <li>${recipe2Data.meals[0].strIngredient6}<li>
+              <li>${recipe2Data.meals[0].strIngredient7}<li>
+              <li>${recipe2Data.meals[0].strIngredient8}<li>
+              <li>${recipe2Data.meals[0].strIngredient9}<li>
+              </ol>
+              </p>
+              <p class="mt-1 text-sm text-gray-500" id="instructions1">${recipe2Data.meals[0].strInstructions}
+              </p>
+          </div>
+      </div>
+  </div>`
 
- 
+    });
+}
+function populateData3() {
+var recipe3 = `https://www.themealdb.com/api/json/v1/1/random.php`;
+fetch(recipe3)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (recipe3Data) {
+    console.log(recipe3Data);
+    recipecard2EL.innerHTML =`<div class="group relative">
+      <div
+          class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img src="${recipe3Data.meals[0].strMealThumb}"
+              alt="Front of men&#039;s Basic Tee in black."
+              class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+      </div>
+      <div class="mt-4 flex justify-between">
+          <div>
+              <h3 class="text-sm text-gray-700">
+                  <span aria-hidden="true" class="absolute inset-0" id="recipeCard1">${recipe3Data.meals[0].strMeal}</span>
+              </h3>
+              <p class="mt-1 text-sm font-medium text-gray-500" id="ingredients1">
+              <h3>Ingredients</h3>
+              <ol>
+              <li>${recipe3Data.meals[0].strIngredient1}<li>
+              <li>${recipe3Data.meals[0].strIngredient2}<li>
+              <li>${recipe3Data.meals[0].strIngredient3}<li>
+              <li>${recipe3Data.meals[0].strIngredient4}<li>
+              <li>${recipe3Data.meals[0].strIngredient5}<li>
+              <li>${recipe3Data.meals[0].strIngredient6}<li>
+              <li>${recipe3Data.meals[0].strIngredient7}<li>
+              <li>${recipe3Data.meals[0].strIngredient8}<li>
+              <li>${recipe3Data.meals[0].strIngredient9}<li>
+              </ol>
+              </p>
+              <p class="mt-1 text-sm text-gray-500" id="instructions1">${recipe3Data.meals[0].strInstructions}
+              </p>
+          </div>
+      </div>
+  </div>`
+  });
+
 }
 
-recipesBtn.addEventListener("click", populateData);
 
 
+recipesBtn.addEventListener("click", populateData1);
+recipecard1El.addEventListener("click", populateData1);
+number1EL.addEventListener("click",populateData1);
+recipecard2EL.addEventListener("click", populateData2);
+number2EL.addEventListener("click",populateData2);
+recipecard3EL.addEventListener("click", populateData3);
+number3EL.addEventListener("click",populateData3);
