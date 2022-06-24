872edc96280f1fad971cf04135c896832e07f6e8
ismail saoudi <ismail.saoudi@icloud.com>
2022-06-24 08:38:38 -0400

    removed files

--- Assets/code.js
+++
@@ -1,4 +0,0 @@
-// define HTMl Static Selectors
-var drinksEl = document.querySelector("#drinks")
-var drinksFormEl = document.querySelector("#drinksForm")
-// Current Searched drink cards selectors
